e0f3bb80c89414486c44fbbc979b919783bc45f4
Metallicus Dev <>
2021-10-06 23:46:09 -0600

    Update submodules

--- proton-test-app/src/components/HelloWorld.vue
+++ proton-test-app/src/components/HelloWorld.vue
@@ -11,102 +11,102 @@
 </template>
 
 <script>
-import { ConnectWallet } from '@bloks/web-sdk'
+import { ConnectWallet } from "@bloks/web-sdk"
 
 // Constants
-const appIdentifier = 'taskly'
+const appIdentifier = "taskly"
 
 export default {
-  name: 'HelloWorld',
+    name: "HelloWorld",
 
-  data () {
-    return {
-      link: undefined,
-      session: undefined
-    }
-  },
-
-  methods: {
-    async createLink ({ restoreSession }) {
-      const { link, session } = await ConnectWallet({
-        linkOptions: {
-          endpoints: ['https://protontestnet.greymass.com'],
-          chainId: '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd',
-          restoreSession,
-          // service: 'https://buoy.ngrok.io'
-        },
-        transportOptions: {
-          requestAccount: 'taskly', // Your proton account
-          requestStatus: true
-        },
-        selectorOptions: {
-          appName: 'Taskly',
-          appLogo: 'https://taskly.protonchain.com/static/media/taskly-logo.ad0bfb0f.svg',
-
-          enabledWalletTypes: ['proton', 'anchor'],
-          customStyleOptions: {
-            modalBackgroundColor: '#F4F7FA',
-            logoBackgroundColor: 'white',
-            isLogoRound: true,
-            optionBackgroundColor: 'white',
-            optionFontColor: 'black',
-            primaryFontColor: 'black',
-            secondaryFontColor: '#6B727F',
-            linkColor: '#752EEB'
-          }
+    data () {
+        return {
+            link: undefined,
+            session: undefined
         }
-      })
-      this.link = link
-      this.session = session
     },
 
-    async login () {
-      // Create link
-      await this.createLink({ restoreSession: false })
-      console.log('User authorization:', this.session.auth) // { actor: 'fred', permission: 'active }
-    },
+    methods: {
+        async createLink ({ restoreSession }) {
+            const { link, session } = await ConnectWallet({
+                linkOptions: {
+                    endpoints: ["https://protontestnet.greymass.com"],
+                    chainId: "71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd",
+                    restoreSession,
+                    // service: 'https://buoy.ngrok.io'
+                },
+                transportOptions: {
+                    requestAccount: "taskly", // Your proton account
+                    requestStatus: true
+                },
+                selectorOptions: {
+                    appName: "Taskly",
+                    appLogo: "https://taskly.protonchain.com/static/media/taskly-logo.ad0bfb0f.svg",
 
-    async transfer () {
-      // Send Transaction
-      const result = await this.session.transact({
-        transaction: {
-          actions: [{
-            // Token contract for XUSDT
-            account: 'xtokens',
-            // Action name
-            name: 'transfer',
-            // Action parameters
-            data: {
-              from: this.session.auth.actor,
-              to: 'syed',
-              quantity: '0.000001 XUSDT',
-              memo: 'Tip!'
-            },
-            authorization: [this.session.auth]
-          }]
+                    enabledWalletTypes: ["proton", "anchor"],
+                    customStyleOptions: {
+                        modalBackgroundColor: "#F4F7FA",
+                        logoBackgroundColor: "white",
+                        isLogoRound: true,
+                        optionBackgroundColor: "white",
+                        optionFontColor: "black",
+                        primaryFontColor: "black",
+                        secondaryFontColor: "#6B727F",
+                        linkColor: "#752EEB"
+                    }
+                }
+            })
+            this.link = link
+            this.session = session
         },
-        broadcast: true
-      })
-      console.log('Transaction ID', result.processed.id)
+
+        async login () {
+            // Create link
+            await this.createLink({ restoreSession: false })
+            console.log("User authorization:", this.session.auth) // { actor: 'fred', permission: 'active }
+        },
+
+        async transfer () {
+            // Send Transaction
+            const result = await this.session.transact({
+                transaction: {
+                    actions: [{
+                        // Token contract for XUSDT
+                        account: "xtokens",
+                        // Action name
+                        name: "transfer",
+                        // Action parameters
+                        data: {
+                            from: this.session.auth.actor,
+                            to: "syed",
+                            quantity: "0.000001 XUSDT",
+                            memo: "Tip!"
+                        },
+                        authorization: [this.session.auth]
+                    }]
+                },
+                broadcast: true
+            })
+            console.log("Transaction ID", result.processed.id)
+        },
+
+        async logout () {
+            await this.link.removeSession(appIdentifier, this.session.auth)
+            this.session = undefined
+        },
+
+        async reconnect () {
+            try {
+                await this.createLink({ restoreSession: true })
+            } catch (e) {
+                console.warn(e)
+            }
+        }
     },
 
-    async logout () {
-      await this.link.removeSession(appIdentifier, this.session.auth)
-      this.session = undefined
-    },
-
-    async reconnect () {
-      try {
-        await this.createLink({ restoreSession: true })
-      } catch (e) {
-        console.warn(e)
-      }
+    created () {
+        this.reconnect()
     }
-  },
-
-  created () {
-    this.reconnect()
-  }
 }
 </script>
 
