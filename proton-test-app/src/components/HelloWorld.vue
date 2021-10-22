553496f953940620c8de219d3dd9cb6dc21d6a0d
Metallicus Dev <>
2021-10-22 10:28:57 -0700

    Add proper submodules

--- proton-test-app/src/components/HelloWorld.vue
+++ proton-test-app/src/components/HelloWorld.vue
@@ -31,7 +31,7 @@ export default {
             const { link, session } = await ConnectWallet({
                 linkOptions: {
                     endpoints: ["https://protontestnet.greymass.com"],
-                    chainId: "71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd",
+                    chainId: "384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0",
                     restoreSession,
                     // service: 'https://buoy.ngrok.io'
                 },
