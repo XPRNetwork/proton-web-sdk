cec170eeacde6ca3bca9c20d40dc6658efe2ce53
jafri <syedjafri97@gmail.com>
2021-12-28 02:55:06 -0700

    Update svelte

---
+++ examples/svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Header from "./Header.svelte";
+	import Transfer from "./Transfer.svelte";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<Header/>
+	<Transfer/>
+</section>
+
+<style>
+</style>
